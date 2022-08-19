293046796d38d9c0b89f1bb139fa33d29b4f8cee
robermau <robertom.perez@outlook.com>
2022-08-18 22:19:24 -0300

    v2

--- adminDePacientes/src/components/Formulario.jsx
+++ adminDePacientes/src/components/Formulario.jsx
@@ -4,9 +4,15 @@ import {useState, useEffect} from 'react'
 
 function Formulario() {
 
-const [nombre , setNombre] = useState('Hook');
+const [nombre , setNombre] = useState('');
+const [propietario , setPropietario] = useState('');
+const [email , setEmail] = useState('');
+const [fecha , setFecha] = useState('');
+const [sintomas , setSintomas] = useState('');
 
-
+const handleSubmit= () => {
+  console.log("Enviando Formulario")
+}
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -17,7 +23,8 @@ const [nombre , setNombre] = useState('Hook');
         <span className="text-indigo-600 font-bold ">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form  onSubmit={handleSubmit}
+       className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota
@@ -28,6 +35,8 @@ const [nombre , setNombre] = useState('Hook');
           type="text"
           placeholder="Nombre de la mascota" 
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          value={nombre}
+          onChange={ (e) => setNombre(e.target.value)}
           />
         </div>
 
